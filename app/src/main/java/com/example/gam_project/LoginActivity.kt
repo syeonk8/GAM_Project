235637147a83b04d0de16d32e903b03c7cc9ae35
ommmdong <dhaehd07@naver.com>
2022-12-08 20:51:30 +0900

    splash추가

--- app/src/main/java/com/example/gam_project/LoginActivity.kt
+++ app/src/main/java/com/example/gam_project/LoginActivity.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.example.gam_project.tracking.MapsActivity
+import com.example.gam_project.MapsActivity
 import com.example.gam_project.tracking.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
